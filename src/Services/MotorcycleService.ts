97c3e1fb3113b185f6510bfb5a8482b548612c9b
Thiago Omoto <thi.omoto@gmail.com>
2023-04-26 19:27:08 -0300

    criando rota /motorcycles, onde é possivel cadastrar uma moto. Obs: Ainda falta a refatoração do Abstract

---
+++ src/Services/MotorcycleService.ts
@@ -0,0 +1,20 @@
+import Motorcycle from '../Domains/Motorcycle';
+import IMotorcycle from '../Interfaces/IMotorcycle';
+import MotorcycleODM from '../Models/MotorcycleODM';
+
+class MotorcycleService {
+  private createMotorcycleDomain(moto: IMotorcycle | null): Motorcycle | null {
+    if (moto) {
+      return new Motorcycle(moto);
+    }
+    return null;
+  }
+
+  public async createMoto(moto: IMotorcycle) {
+    const motoODM = new MotorcycleODM();
+    const newMoto = await motoODM.create(moto);
+    return this.createMotorcycleDomain(newMoto);
+  }
+}
+
+export default MotorcycleService;
