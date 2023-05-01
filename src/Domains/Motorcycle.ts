d572640a4d7bdc6446a09d5a19efaf3c7b0bb7d5
Thiago Omoto <thi.omoto@gmail.com>
2023-05-01 16:42:58 -0300

    refatorando interfaces e a domain de carros e motos

--- src/Domains/Motorcycle.ts
+++ src/Domains/Motorcycle.ts
@@ -1,72 +1,14 @@
 import IMotorcycle from '../Interfaces/IMotorcycle';
+import Vehicle from './Vehicle';
 
-class Motorcycle {
-  protected id: string | undefined;
-  protected model: string;
-  protected year: number;
-  protected color: string;
-  protected status: boolean | undefined;
-  protected buyValue: number;
+class Motorcycle extends Vehicle {
   private category: string;
   private engineCapacity: number;
 
   constructor(motorcycle: IMotorcycle) {
-    this.id = motorcycle.id;
-    this.model = motorcycle.model;
-    this.year = motorcycle.year;
-    this.color = motorcycle.color;
-    this.status = motorcycle.status;
-    this.buyValue = motorcycle.buyValue;
+    super(motorcycle);
     this.category = motorcycle.category;
     this.engineCapacity = motorcycle.engineCapacity;
-  }
-
-  public setId(id: string) {
-    this.id = id;
-  }
-
-  public getId() {
-    return this.id;
-  }
-
-  public setModel(model: string) {
-    this.model = model;
-  }
-
-  public getModel() {
-    return this.model;
-  }
-
-  public setYear(year: number) {
-    this.year = year;
-  }
-
-  public getYear() {
-    return this.year;
-  }
-
-  public setColor(color: string) {
-    this.color = color;
-  }
-
-  public getColor() {
-    return this.color;
-  }
-
-  public setStatus(status: boolean) {
-    this.status = status;
-  }
-
-  public getStatus() {
-    return this.status;
-  }
-
-  public setBuyValue(buyValue: number) {
-    this.buyValue = buyValue;
-  }
-
-  public getBuyValue() {
-    return this.buyValue;
   }
 
   public setCategory(category: string) {
